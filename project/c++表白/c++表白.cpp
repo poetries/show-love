fbed9cf503217b3f44c9c7d08145011b8fd2e5f9
poetries <jingguanliuye@gmail.com>
2016-07-19 01:47:38 +0800

    add some files

---
+++ project/c++表白/c++表白.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+#define A 800
+#define B 400
+#define C 200
+#define D 150
+
+#define L1 262
+#define L2 294
+#define L3 330
+#define L4 349
+#define L5 392
+#define L6 440
+#define L7 493
+
+#define N1 532
+#define N2 588
+#define N3 660
+#define N4 698
+#define N5 784
+#define N6 880
+#define N7 988
+
+#define H1 1046
+#define H2 1175
+#define H3 1319
+#define H4 1397
+#define H5 1568
+#define H6 1760
+#define H7 1976
+
+void sung(){
+cout<<".";Beep(N3,A);cout<<".";Beep(N3,A);cout<<".";Beep(N3,A);cout<<".";Beep(N3,C);cout<<".";Beep(N2,C);cout<<".";Beep(N1,B);
+cout<<".";Beep(N2,A*2);cout<<".";Beep(N2,B);cout<<".";Beep(N5,B);cout<<".";Beep(N2,B);cout<<".";Beep(N1,C);cout<<".";Beep(N2,C);
+cout<<".";Beep(N3,A);cout<<".";Beep(N4,A);cout<<".";Beep(N4,A);cout<<".";Beep(N6,B);cout<<".";Beep(N4,B);cout<<".\n\n ";Beep(N2,A*2);
+
+
+cout<<"我 ";Beep(N1,B);cout<<"以 ";Beep(N2,B);cout<<"为 ";Beep(N3,A);cout<<"要 ";Beep(N2,B);cout<<"是 ";Beep(N1,B);
+cout<<"唱 ";Beep(L7,A);cout<<"的 ";Beep(L5,A);cout<<"用 ";Beep(N1,A);cout<<"心 ";Beep(L7,B);cout<<"良 ";Beep(N1,B);cout<<"苦";Beep(L5,A*2);
+cout<<"\n\n 你 ";Beep(N1,B);cout<<"总 ";Beep(L7,B);cout<<"会 ";Beep(N1,B);cout<<"对 ";Beep(N5,A);cout<<"我 ";Beep(N1,A);
+cout<<"多 ";Beep(N1,A);cout<<"点 ";Beep(L7,B);cout<<"在 ";Beep(N1,B);cout<<"乎 \n\n ";Beep(N2,A);
+
+cout<<"我 ";Beep(N1,B);cout<<"以 ";Beep(N2,B);cout<<"为 ";Beep(N3,A);cout<<"虽 ";Beep(N2,B);cout<<"然 ";Beep(N1,B);
+cout<<"爱 ";Beep(L7,A);cout<<"情 ";Beep(L5,A);cout<<"已 ";Beep(N1,A);cout<<"成 ";Beep(L7,B);cout<<"往 ";Beep(N1,B);cout<<"事";Beep(N5,A);
+
+cout<<"\n\n 千 ";Beep(N6,B);cout<<"言 ";Beep(N4,B);cout<<"万 ";Beep(N3,B);cout<<"语 ";Beep(N2,B);cout<<"说 ";Beep(N4,B);cout<<"出 ";Beep(N3,B);cout<<"来 ";Beep(N2,B);
+cout<<"可 ";Beep(N1,B);cout<<"以 ";Beep(L5,B);cout<<"互 ";Beep(L6,A);cout<<"相 ";Beep(N1,B);cout<<"安 ";Beep(N2,B);cout<<"抚";Beep(N1,A);
+
+Beep(00,B);
+cout<<"\n\n 期 ";Beep(N1,B);cout<<"待 ";Beep(N3,B);cout<<"你 ";Beep(N5,B);cout<<"感 ";Beep(N7,A);cout<<"动";Beep(H2,A);
+cout<<"\n\n 真 ";Beep(H1,A);cout<<"实 ";Beep(N5,B);cout<<"的 ";Beep(N3,B);
+cout<<"我 ";Beep(N4,B);cout<<"们 ";Beep(N4,B);cout<<"难 ";Beep(N5,B);cout<<"相 ";Beep(N6,C);cout<<"处";Beep(N5,C);
+
+Beep(00,B); 
+cout<<"\n\n 写 ";Beep(L6,B);cout<<"词 ";Beep(N1,B);cout<<"的 ";Beep(N3,B);cout<<"让 ";Beep(N5,A);cout<<"我";Beep(N6,A);
+cout<<"\n\n 唱 ";Beep(N5,A);cout<<"出 ";Beep(N3,B);cout<<"你 ";Beep(N1,B);cout<<"要 ";Beep(N4,A);cout<<"的 ";Beep(N3,B);cout<<"幸 ";Beep(N1,C);cout<<"福";Beep(N2,C);
+
+Beep(00,B);
+cout<<"\n\n 谁 ";Beep(N1,B);cout<<"曾 ";Beep(N3,B);cout<<"今 ";Beep(N5,B);cout<<"感 ";Beep(N7,A);cout<<"动 ";Beep(H2,A);
+cout<<"\n\n 分 ";Beep(H1,A);cout<<"手 ";Beep(N7,B);cout<<"的 ";Beep(H1,B);cout<<"关 ";Beep(H2,B);cout<<"头 ";Beep(H1,B);
+cout<<"才 ";Beep(N5,B);cout<<"懂 ";Beep(N3,C);cout<<"得 ";Beep(N1,C);
+
+Beep(00,B);
+cout<<"\n\n 离 ";Beep(N1,B);cout<<"开 ";Beep(N2,B);cout<<"排 ";Beep(N3,B);cout<<"行 ";Beep(N3,A);cout<<"榜 ";Beep(L6,A);
+cout<<"更 ";Beep(N4,B);cout<<"铭 ";Beep(N3,B);cout<<"心 ";Beep(N4,B);cout<<"刻 ";Beep(N6,B);cout<<"骨 ";Beep(N5,A*1.5);
+
+Beep(00,B);
+cout<<"\n\n 我 ";Beep(N5,B);cout<<"已 ";Beep(N6,B);cout<<"经 ";Beep(N7,B);cout<<"相 ";Beep(H3,A);cout<<"信 \n\n ";Beep(H1,A);
+cout<<"有 ";Beep(N7,B);cout<<"些 ";Beep(N5,B);cout<<"人 ";Beep(N6,B);cout<<"我 ";Beep(N7,B);cout<<"永 ";Beep(N6,B);cout<<"远 ";Beep(N7,B);
+cout<<"不 ";Beep(N6,B);cout<<"必 ";Beep(N5,C);cout<<"等 ";Beep(N3,C);
+
+Beep(00,B); 
+cout<<"\n\n 所 ";Beep(N6,B);cout<<"以 ";Beep(N7,B);cout<<"我 ";Beep(H1,B);cout<<"明 ";Beep(H4,A);cout<<"白 ";Beep(H1,B);
+cout<<"在 ";Beep(H1,B);cout<<"灯 ";Beep(H3,B);cout<<"火 ";Beep(H2,B);cout<<"阑 ";Beep(H1,B);cout<<"珊 ";Beep(N6,B);cout<<"处 ";Beep(N4,B);
+cout<<"为 ";Beep(N4,B);cout<<"什 ";Beep(N6,D);cout<<"么 ";Beep(H1,D);cout<<"会 ";Beep(H3,D);cout<<"哭";Beep(H2,B);
+
+cout<<"\n\n 你 ";Beep(N5,B);cout<<"不 ";Beep(N6,B);cout<<"会 ";Beep(N7,B);cout<<"相 ";Beep(H3,A);cout<<"信 \n\n ";Beep(H1,A);
+cout<<"嫁 ";Beep(N7,B);cout<<"给 ";Beep(N5,B);cout<<"我 ";Beep(N6,B);cout<<"明 ";Beep(N7,B);cout<<"天 ";Beep(H3,B);
+cout<<"有 ";Beep(H2,B);cout<<"多 ";Beep(H1,B);cout<<"幸 ";Beep(N7,C);cout<<"福 ";Beep(N3,C);
+
+Beep(00,B); 
+cout<<"\n\n 只 ";Beep(N6,B);cout<<"想 ";Beep(N7,B);cout<<"你 ";Beep(H1,B);cout<<"明 ";Beep(H4,A);cout<<"白 \n\n ";Beep(H1,B);
+cout<<"我 ";Beep(H1,B);cout<<"心 ";Beep(H3,B);cout<<"甘 ";Beep(H2,B);cout<<"情 ";Beep(H1,B);cout<<"愿 ";Beep(N6,B);
+cout<<"爱 ";Beep(H1,D);cout<<"爱 ";Beep(H1,D);cout<<"爱 ";Beep(H1,D);cout<<"爱 ";Beep(H1,D);cout<<"到 ";Beep(H2,D);cout<<"要 ";Beep(H3,D);cout<<"吐";Beep(H2,A);
+
+cout<<"\n\n 那 ";Beep(H2,B);cout<<"是 ";Beep(H1,B);cout<<"醉 ";Beep(N7,A);cout<<"生 ";Beep(H1,A);cout<<"梦 ";Beep(N7,A);cout<<"死 ";Beep(N5,A);
+cout<<"才 ";Beep(N6,B);cout<<"能 ";Beep(N6,B);cout<<"熬 ";Beep(N6,D);cout<<"成 ";Beep(N5,D);cout<<"的 ";Beep(N3,D);cout<<"苦";Beep(N1,A*1.5);
+
+Beep(00,B);
+cout<<"\n\n 爱 ";Beep(N1,B);cout<<"如 ";Beep(N2,B);cout<<"潮 ";Beep(N3,B);cout<<"水";Beep(N4,A);
+cout<<"\n\n 我 ";Beep(N4,D);cout<<"忘 ";Beep(H1,D);cout<<"了 ";Beep(N7,D);cout<<"我 ";Beep(N5,A);cout<<"是 ";Beep(N1,A);cout<<"谁\n\n ";Beep(N4,A);
+cout<<"至 ";Beep(N4,D);cout<<"少 ";Beep(N4,D);cout<<"还 ";Beep(N3,D);cout<<"有 ";Beep(L6,A);cout<<"你 ";Beep(L7,A);cout<<"哭";Beep(N1,A*3);
+
+
+Beep(00,B);
+cout<<"\n\n .";Beep(N5,B);cout<<".";Beep(N5,D);cout<<".";Beep(N4,D);cout<<".";Beep(N3,D);
+cout<<".";Beep(H3,D);cout<<".";Beep(H3,C);cout<<".";Beep(H3,C);cout<<".";Beep(H4,C);cout<<".";
+Beep(H3,A);cout<<".";Beep(H3,A);cout<<".";Beep(H2,B);cout<<".";Beep(H1,B);cout<<".";Beep(H1,A);
+
+cout<<"\n\n 我 ";Beep(N1,B);cout<<"想 ";Beep(N2,B);cout<<"唱 ";Beep(N3,A);cout<<"一 ";Beep(N2,B);cout<<"首 ";Beep(N1,B);cout<<"歌 ";Beep(L7,A);
+cout<<"给 ";Beep(L5,A);cout<<"我 ";Beep(N1,A);cout<<"们 ";Beep(L7,B);cout<<"祝 ";Beep(N1,B);cout<<"福";Beep(L5,A);
+cout<<"\n\n 唱 ";Beep(N1,A);cout<<"完 ";Beep(L7,B);cout<<"了 ";Beep(N1,B);cout<<"我 ";Beep(L5,A);cout<<"会 ";Beep(N1,A);
+cout<<"一 ";Beep(N1,A);cout<<"个 ";Beep(L7,B);cout<<"人 ";Beep(N1,B);cout<<"住";Beep(N2,A);
+
+cout<<"\n\n 我 ";Beep(N1,B);cout<<"愿 ";Beep(N2,B);cout<<"意 ";Beep(N3,A);
+cout<<"试 ";Beep(N2,B);cout<<"着 ";Beep(N1,B);cout<<"了 ";Beep(L7,A);cout<<"解 ";Beep(L5,A);
+cout<<"从 ";Beep(N1,A);cout<<"此 ";Beep(L7,B);cout<<"以 ";Beep(N1,B);cout<<"后";Beep(N5,A);
+cout<<"\n\n 拥 ";Beep(N6,A);cout<<"挤 ";Beep(N4,B);cout<<"的 ";Beep(N3,B);cout<<"房 ";Beep(N2,B);cout<<"间 ";Beep(N4,B);
+
+cout<<"一 ";Beep(N3,B);cout<<"个 ";Beep(N2,B);cout<<"人 ";Beep(N1,B);cout<<"的 ";Beep(N3,B);cout<<"心 ";Beep(N2,A);
+cout<<"有 ";Beep(N3,C);cout<<"多 ";Beep(N4,C);cout<<"孤 ";Beep(H1,C);cout<<"独";Beep(N5,B);
+
+Beep(00,B);
+cout<<"\n\n 我 ";Beep(N5,B);cout<<"已 ";Beep(N6,B);cout<<"经 ";Beep(N7,B);cout<<"相 ";Beep(H3,A);cout<<"信 ";Beep(H1,A);
+cout<<"\n\n 有 ";Beep(N7,B);cout<<"些 ";Beep(N5,B);cout<<"人 ";Beep(N6,B);cout<<"我 ";Beep(N7,B);cout<<"永 ";Beep(N6,B);cout<<"远 ";Beep(N7,B);
+cout<<"不 ";Beep(N6,B);cout<<"必 ";Beep(N5,C);cout<<"等 ";Beep(N3,C);
+
+Beep(00,B); 
+cout<<"\n\n 所 ";Beep(N6,B);cout<<"以 ";Beep(N7,B);cout<<"我 ";Beep(H1,B);cout<<"明 ";Beep(H4,A);cout<<"白 ";Beep(H1,B);
+
+cout<<"在 ";Beep(H1,B);cout<<"灯 ";Beep(H3,B);cout<<"火 ";Beep(H2,B);cout<<"阑 ";Beep(H1,B);cout<<"珊 ";Beep(N6,B);cout<<"处 ";Beep(N4,B);
+cout<<"为 ";Beep(N4,B);cout<<"什 ";Beep(N6,D);cout<<"么 ";Beep(H1,D);cout<<"会 ";Beep(H3,D);cout<<"哭";Beep(H2,B);
+
+cout<<"\n\n 你 ";Beep(N5,B);cout<<"不 ";Beep(N6,B);cout<<"会 ";Beep(N7,B);cout<<"相 ";Beep(H3,A);cout<<"信 ";Beep(H1,A);
+cout<<"\n\n 嫁 ";Beep(N7,B);cout<<"给 ";Beep(N5,B);cout<<"我 ";Beep(N6,B);cout<<"明 ";Beep(N7,B);cout<<"天 ";Beep(H3,B);
+cout<<"有 ";Beep(H2,B);cout<<"多 ";Beep(H1,B);cout<<"幸 ";Beep(N7,C);cout<<"福 ";Beep(N3,C);
+
+Beep(00,B); 
+cout<<"\n\n 只 ";Beep(N6,B);cout<<"想 ";Beep(N7,B);cout<<"你 ";Beep(H1,B);cout<<"明 ";Beep(H4,A);cout<<"白 ";Beep(H1,B);
+cout<<"\n\n 我 ";Beep(H1,B);cout<<"心 ";Beep(H3,B);cout<<"甘 ";Beep(H2,B);cout<<"情 ";Beep(H1,B);cout<<"愿 ";Beep(N6,B);
+cout<<"爱 ";Beep(H1,D);cout<<"爱 ";Beep(H1,D);cout<<"爱 ";Beep(H1,D);cout<<"爱 ";Beep(H1,D);cout<<"到 ";Beep(H2,D);cout<<"要 ";Beep(H3,D);cout<<"吐";Beep(H2,A);
+
+cout<<"\n\n 让 ";Beep(H2,B);cout<<"我 ";Beep(H1,B);cout<<"断 ";Beep(N7,D);cout<<"了 ";Beep(N7,D);cout<<"气 ";Beep(H1,D);
+cout<<"铁 ";Beep(N7,D);cout<<"了 ";Beep(N7,D);cout<<"心 ";Beep(H3,D);cout<<"爱 ";Beep(N7,D);cout<<"的 ";Beep(N7,D);cout<<"过 ";Beep(N6,D);cout<<"火";Beep(N5,C);
+
+cout<<"\n\n 一 ";Beep(N5,C);cout<<"回 ";Beep(N6,D);cout<<"头 ";Beep(H1,D);
+cout<<"就 ";Beep(H1,D);cout<<"找 ";Beep(H1,D);cout<<"到 ";Beep(H2,D);cout<<"出 ";Beep(H1,D);cout<<"路";Beep(H3,A);
+
+cout<<"\n\n 让 ";Beep(H2,C);cout<<"我 ";Beep(H1,D);cout<<"成 ";Beep(N7,D);cout<<"为 ";Beep(N7,D);cout<<"了 ";Beep(H1,D);
+cout<<"无 ";Beep(N7,D);cout<<"情 ";Beep(N7,D);cout<<"的 ";Beep(H3,D);cout<<"K ";Beep(N7,D);cout<<"歌 ";Beep(N7,D);cout<<"之 ";Beep(N6,D);cout<<"王";Beep(N5,C);
+cout<<"\n\n 麦 ";Beep(N5,D);cout<<"克 ";Beep(N6,D);cout<<"风 ";Beep(N6,D);cout<<"都 ";Beep(N6,D);cout<<"让 ";Beep(N6,D);cout<<"我 ";Beep(N5,D);
+cout<<"征 ";Beep(N3,D);cout<<"服 ";Beep(N1,A);
+
+cout<<"\n\n 想 ";Beep(N1,D);cout<<"不 ";Beep(N2,D);cout<<"到 ";Beep(N3,D);
+cout<<"你 ";Beep(N4,A);cout<<"若 ";Beep(N4,D);cout<<"无 ";Beep(H1,D);cout<<"其 ";Beep(N7,D);cout<<"事 ";Beep(N5,A);cout<<"的 ";Beep(N1,A);cout<<"说";Beep(N4,A);
+cout<<"\n\n 这 ";Beep(N4,D);cout<<"样 ";Beep(N4,D);cout<<"滥 ";Beep(N3,D);cout<<"情\n\n ";Beep(L6,A);cout<<"何 ";Beep(L7,A);cout<<"苦";Beep(N1,A);
+
+Beep(00,A);
+cout<<"\n\n 我 ";Beep(N1,B);cout<<"想 ";Beep(N2,B);cout<<"来 ";Beep(N3,A);cout<<"一 ";Beep(N2,B);cout<<"个 ";Beep(N1,B);
+cout<<"吻 ";Beep(L7,A);cout<<"别 ";Beep(L5,A);cout<<"做 ";Beep(N1,A);cout<<"为 ";Beep(L7,B);cout<<"结 ";Beep(N1,B);cout<<"束";Beep(N5,A);
+cout<<"\n\n 想 ";Beep(N1,A);cout<<"不 ";Beep(N4,B);cout<<"到 ";Beep(N4,B);cout<<"你 ";Beep(N5,B);cout<<"只 ";Beep(N4,B);cout<<"说 ";Beep(N3,B);
+cout<<"我 ";Beep(N4,B);cout<<"不 ";Beep(N3,B);cout<<"许 ";Beep(L5,B);cout<<"哭";Beep(L6,A);
+cout<<"\n\n 不 ";Beep(L6,A);cout<<"让 ";Beep(N1,D);cout<<"我 ";Beep(N3,D);cout<<"领 ";Beep(N7,D);cout<<"悟\n\n ";Beep(H1,A*1.5);cout<<".";
+}
+
+void main()
+{
+sung();
+}
---
+++ project/c表白/c语言表白.cpp
@@ -0,0 +1,245 @@
+#include<iostream.h>
+#include<windows.h>
+#include<stdio.h>
+#define stoptimeshort  80
+#define stoptimelong   150
+void main()
+{
+	//////////////////
+	char ch[10];
+	int f[9][36]={  0,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	                0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,
+	                0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0
+	             };
+	int s[9][21]={  0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,
+	                0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0,0,0,0,1,0,
+	                1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,
+	                1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,
+	                0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,
+	                0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,
+	                0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,
+	                0,0,0,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,
+	                0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,
+	             };
+	int t[9][22]={  0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,
+	                0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,
+	                1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,
+	                1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
+	                0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,
+	                0,0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,
+	                0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,
+	                0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,
+	                0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,
+	             };
+	system ( "title I Love You" );
+	//////////////////
+	printf ( "%s","        " );
+	Sleep ( stoptimelong );
+	printf ( "%s","其" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","实\n" );
+	////////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 72" );
+	Sleep ( stoptimelong );
+	printf ( "%s","有" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","一" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","句" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","话\n" );
+	///////////////////////////
+	printf ( "%s","        " );
+	system ( "color 79" );
+	Sleep ( stoptimelong );
+	printf ( "%s","一" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","直" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","藏" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","在" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","心" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","里\n" );
+	/////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 7c" );
+	Sleep ( stoptimelong );
+	printf ( "%s","可" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","是\n" );
+	///////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 7d" );
+	Sleep ( stoptimelong );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","没" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","有" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","勇" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","气" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","对" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","你" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","说\n" );
+	///////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 7e" );
+	Sleep ( stoptimelong );
+	printf ( "%s","今" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","天\n" );
+	//////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 7a" );
+	Sleep ( stoptimelong );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","终" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","于" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","鼓" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","起" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","勇" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","气\n" );
+	///////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 7b" );
+	Sleep ( stoptimelong );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","要" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","对" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","你" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","说:\n" );
+	///////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 79" );
+	Sleep ( stoptimelong );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","…\n" );
+	/////////////////////////////////
+	printf ( "%s","        " );
+	system ( "color 72" );
+	Sleep ( stoptimelong );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","…" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","…\n" );
+	//////////////////////////////////
+	Sleep ( stoptimelong );
+	system ( "color fc" );
+	for ( int i=0; i<9; i++ )
+	{
+		cout<<"        ";
+		for ( int j=0; j<36; j++ )
+		{
+			if ( f[i][j]==0 )
+				cout<<" ";
+			else
+				cout<<"*";
+		}
+		cout<<endl;
+		Sleep ( 100 );
+	}
+	/////////////////////////////////////
+line:
+	printf ( "%s","        " );
+	Sleep ( stoptimelong );
+	printf ( "%s","你" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","愿" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","意" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","接" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","受" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","我" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","吗" );
+	Sleep ( stoptimeshort );
+	printf ( "%s","?(Y/N)" );
+	////////////////////////////////////
+	cout<<endl;
+	cout<<"        ";
+	cin>>ch;
+	Sleep ( stoptimelong );
+	if ( ch[0]=='y'||ch[0]=='Y' )
+	{
+		system ( "color fc" );
+		for ( int i=0; i<9; i++ )
+		{
+			cout<<"        ";
+			for ( int j=0; j<21; j++ )
+			{
+				if ( s[i][j]==0 )
+					cout<<" ";
+				else
+					cout<<"*";
+			}
+			cout<<endl;
+			Sleep ( 100 );
+		}
+		Sleep ( 3000 );
+	}
+	else
+	{
+		if ( ch[0]=='n'||ch[0]=='N' )
+		{
+			system ( "color 0f" );
+			for ( int i=0; i<9; i++ )
+			{
+				cout<<"        ";
+				for ( int j=0; j<22; j++ )
+				{
+					if ( t[i][j]==0 )
+						cout<<" ";
+					else
+						cout<<"*";
+				}
+				cout<<endl;
+				Sleep ( 100 );
+			}
+			printf ( "%s","        " );
+			Sleep ( stoptimelong );
+			printf ( "%s","心" );
+			Sleep ( stoptimeshort );
+			printf ( "%s","碎" );
+			Sleep ( stoptimeshort );
+			printf ( "%s","了" );
+			Sleep ( stoptimeshort );
+			printf ( "%s","……\n" );
+		}
+		else
+			goto line;
+	}
+	system ( "color 0f" );
+	system ( "pause" );
+}
